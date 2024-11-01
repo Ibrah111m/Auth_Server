2fac4aaf684727ce2e4afcb980771939561d75c8
Ibrahim <144321381+Ibrah111m@users.noreply.github.com>
2024-11-01 10:03:09 +0100

    Added JWT authentication utilites: access token and refresh token generation and validation and CSRF token generation

--- src/domain/auth_handler.js
+++ src/domain/auth_handler.js
@@ -1,19 +1,39 @@
-const {ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET} = require('../config');
-
 const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
+const {getUserGroups} = require('./user_handler');
 
-const generateAccessToken = (user) => {
- return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+
+const SECRET_KEY = 'your_secret_key';
+const REFRESH_SECRET_KEY = 'your_refresh_secret_key';
+
+exports.generateAccessToken = (user) => {
+  const payload = {
+    id: user.id,
+    username: user.username,
+    groups: getUserGroups(user.id)
+  };
+  return jwt.sign(payload, SECRET_KEY, {expiresIn: '15m'});
 };
 
-const generateRefreshToken = (user) => {
-  return jwt.sign(user, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+exports.generateRefreshToken = (user) => {
+  return jwt.sign({id : user.id}, REFRESH_SECRET_KEY, {expiresIn: '7d'});
 };
 
-const generateCsrfToken = () => {
- return crypto.randomBytes(16).toString('hex');
+exports.generateCsrfToken = () => {
+  return require('crypto').randomBytes(64).toString('hex');
 };
 
-module.exports = { generateAccessToken, generateRefreshToken, generateCsrfToken };
+exports.validateAccessToken = (token) => {
+  try {
+    return jwt.verify(token, SECRET_KEY);
+  } catch (error) {
+    return null;
+  }
+};
 
+exports.validateRefreshToken = (token) => {
+  try {
+    return jwt.verify(token, REFRESH_SECRET_KEY);
+  } catch (error) {
+    return null;
+  }
+};
